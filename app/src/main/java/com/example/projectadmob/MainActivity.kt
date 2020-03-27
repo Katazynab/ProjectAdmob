720bf178d5e5b1a873fe9a2caf81ada5f8b4b313
Katazynab <katazyna.balcevic@gmail.com>
2020-03-27 15:34:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/projectadmob/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.projectadmob
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+
+class MainActivity : AppCompatActivity() {
+    lateinit var adview: AdView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        MobileAds.initialize(this, "ca-app-pub-6971339187996673/1331823730")
+
+        adview = findViewById(R.id.banner)
+        val adRequest: AdRequest = AdRequest.Builder().build()
+        adview.loadAd(adRequest)
+    }
+}
